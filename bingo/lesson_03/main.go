40b62147611996e7724127c2acbc0bd1d6527d71
K-Philleani <964202623@qq.com>
2020-10-14 11:21:32 +0800

    lesson_03

---
+++ bingo/lesson_03/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	// if
+	score := 65
+	if score >= 90 {
+		fmt.Println("A")
+	} else if score >75 {
+		fmt.Println("B")
+	} else {
+		fmt.Println("C")
+	}
+
+	if grade := 90; grade >= 90 {
+		fmt.Println("A")
+	} else if score >75 {
+		fmt.Println("B")
+	} else {
+		fmt.Println("C")
+	}
+
+	// switch
+	age := 2
+	switch age {
+		case 1:
+			fmt.Println("1")
+		case 2:
+			fmt.Println("2")
+		default:
+			fmt.Println("unknown")
+	}
+
+}
